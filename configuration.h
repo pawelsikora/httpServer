a7b27f532b714fcf06fc83cd7b975ee29acd8873
pawelsikora <sikor6@gmail.com>
2016-05-04 22:52:04 +0200

    adding configuration.h header to handle configuration of the server. In includes all defs.h now

---
+++ configuration.h
@@ -0,0 +1,20 @@
+#include "defs.h"
+
+struct server_config 
+{
+	char root[MAX_SIZE_OF_PATH];
+	char logfile[MAX_SIZE_OF_FILE_NAME];
+	char lockfile[MAX_SIZE_OF_FILE_NAME];
+
+	struct port_conf
+	{
+		int  	 val;
+		char 	 name[MAX_SIZE_OF_PORT];
+	} port;
+
+	struct daemon_var {
+		char	 str[SIZE_OF_ARRAY_ONE_EL];
+		int	 val;
+	} setDaemon;
+
+} configuration;
